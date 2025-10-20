3f24fa30e3f383ea031752898877b847413808f9
EricSpencer00 <espencer2@luc.edu>
2025-10-20 11:53:45 -0500

    height size and scale

--- src/main/scala/shapes.scala
+++ src/main/scala/shapes.scala
@@ -8,4 +8,29 @@ enum Shape derives CanEqual:
   case Group(shapes: Shape*)
 
 end Shape
+
+def height(s: Shape): Int = s match
+  case Shape.Rectangle(_, h) => h
+  case Shape.Location(_, _, sh) => height(sh)
+  case Shape.Ellipse(_, ry) => 2 * ry
+  case Shape.Group(shapes @ _*) =>
+    if shapes.isEmpty then 0
+    else shapes.map(height).max
+
+def size(s: Shape): Int = s match
+  case Shape.Rectangle(w, h) => w * h
+  case Shape.Location(_, _, sh) => size(sh)
+  case Shape.Ellipse(rx, ry) => Math.PI.toInt * rx * ry
+  case Shape.Group(shapes @ _*) =>
+    shapes.map(size).sum
+
+def scale(s: Shape, factor: Double): Shape = s match
+  case Shape.Rectangle(w, h) =>
+    Shape.Rectangle((w * factor).toInt, (h * factor).toInt)
+  case Shape.Location(x, y, sh) =>
+    Shape.Location((x * factor).toInt, (y * factor).toInt, scale(sh, factor))
+  case Shape.Ellipse(rx, ry) =>
+    Shape.Ellipse((rx * factor).toInt, (ry * factor).toInt)
+  case Shape.Group(shapes @ _*) =>
+    Shape.Group(shapes.map(sh => scale(sh, factor))*)
   
